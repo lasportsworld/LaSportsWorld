8306e79488c26273872208b5607d9506a86c9c3c
lasportsworld <eitan@lasportsworld.com>
2026-07-06 01:56:44 -0700

    Add temporary lead capture landing pages

---
+++ src/app/site-preview/page.tsx
@@ -0,0 +1,23 @@
+import Hero from "@/components/home/Hero";
+import AboutSection from "@/components/home/AboutSection";
+import ProgramsSection from "@/components/home/ProgramsSection";
+import WhyUsSection from "@/components/home/WhyUsSection";
+import HowItWorksSection from "@/components/home/HowItWorksSection";
+import GalleryStripSection from "@/components/home/GalleryStripSection";
+import TestimonialsSection from "@/components/home/TestimonialsSection";
+import CTASection from "@/components/home/CTASection";
+
+export default function SitePreviewPage() {
+  return (
+    <>
+      <Hero />
+      <AboutSection />
+      <ProgramsSection />
+      <WhyUsSection />
+      <HowItWorksSection />
+      <GalleryStripSection />
+      <TestimonialsSection />
+      <CTASection />
+    </>
+  );
+}
